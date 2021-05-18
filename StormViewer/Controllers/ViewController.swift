82bf9db92f3481782bae9b7c73fd2fd31e1fe6e5
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-05-18 11:14:46 +0900

    First phase

--- StormViewer/Controllers/ViewController.swift
+++ StormViewer/Controllers/ViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures: [String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fileManager: FileManager = FileManager()
+        let path: String = Bundle.main.resourcePath!
+        let items: [String] = try! fileManager.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                self.pictures.append(item)
+            }
+        }
     }
 }
 
